625a011923be60a4bcd42233e98892a8192c13a3
Trình Công Trình <congtrinh097@gmail.com>
2018-04-03 11:20:32 +0700

    Add tab screen

--- src/Screens/PointRecord/PointRecord.tsx
+++ src/Screens/PointRecord/PointRecord.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
-import {StatusBar, StyleSheet, View, Text, Button} from 'react-native';
+import {StatusBar, StyleSheet, View, Text} from 'react-native';
 import {ScreenName} from "../../common/constains";
+import {Tab, Tabs, Header, Icon} from "native-base";
+import {Colors} from "../../common/variables";
+import {Tab1} from "./Tabs/Tab1";
+import {Tab3} from "./Tabs/Tab3";
+import {Tab2} from "./Tabs/Tab2";
+
+const Button: any = require('native-base').Button;
 
 interface thisProps {
   navigation: any
@@ -9,23 +16,85 @@ interface thisProps {
 interface thisState {
 
 }
+
+
 export class PointRecordScreen extends React.Component<thisProps, thisState> {
   static navigationOptions: any = {
     header: null,
   };
 
+  renderHeader() {
+    return (
+      <View style={{backgroundColor: Colors.RedStrong, flexDirection: 'row'}}>
+        <View style={{
+          height: 60, width: 60, backgroundColor: Colors.RedStrong, justifyContent: 'center', alignItems: 'center',
+          position: 'absolute', left: 0
+        }}>
+          <Button
+            transparent
+            onPress={() => this.props.navigation.navigate("DrawerOpen")}
+          >
+            <Icon name="ios-menu" style={{color: '#fff', fontSize: 30}}/>
+          </Button>
+        </View>
+        <View style={{flex: 1, height: 60, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{textAlign: 'center', fontSize: 16, fontWeight: 'bold', color: '#fff'}}>Ghi điểm tiến lên</Text>
+        </View>
+      </View>
+    );
+  }
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Màn hình ghi điểm</Text>
-        <Button
-          title="Quay lại"
-          onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            this.props.navigation.goBack()
-          }}
-        />
+        {this.renderHeader()}
+          <Tabs initialPage={1}>
+            <Tab heading="Ghi điểm"
+                 tabStyle={styles.tabHeader}
+                 textStyle={styles.tabHeaderText}
+                 activeTabStyle={styles.activeTabHeader}
+                 activeTextStyle={styles.activeTabHeaderText}
+            >
+              <Tab1/>
+            </Tab>
+            <Tab heading="Kết quả"
+                 tabStyle={styles.tabHeader}
+                 textStyle={styles.tabHeaderText}
+                 activeTabStyle={styles.activeTabHeader}
+                 activeTextStyle={styles.activeTabHeaderText}
+            >
+              <Tab2/>
+            </Tab>
+            <Tab heading="Chi tiết"
+                 tabStyle={styles.tabHeader}
+                 textStyle={styles.tabHeaderText}
+                 activeTabStyle={styles.activeTabHeader}
+                 activeTextStyle={styles.activeTabHeaderText}
+
+            >
+              <Tab3/>
+            </Tab>
+          </Tabs>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  tabHeader: {
+    backgroundColor: Colors.White,
+    borderBottomColor: Colors.RedStrong,
+    borderBottomWidth: 2
+  },
+  tabHeaderText: {
+    color: Colors.Gray,
+  },
+  activeTabHeader: {
+    backgroundColor: Colors.White,
+    borderBottomColor: Colors.RedStrong,
+    borderBottomWidth: 4,
+  },
+  activeTabHeaderText: {
+    color: Colors.Black,
+  }
+});
